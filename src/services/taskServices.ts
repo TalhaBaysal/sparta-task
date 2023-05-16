e99ebf96c6dc0e521691f9760b8409064a22d2e0
Talha <talha.baysall@hotmail.com>
2023-05-16 15:36:41 +0300

    bringing back axiosInstance

--- src/services/taskServices.ts
+++ src/services/taskServices.ts
@@ -1,38 +1,38 @@
 import axios from "axios";
-// import { axiosInstance } from "../api/axios.instance";
+import { axiosInstance } from "../api/axios.instance";
 import { QueryFunctionContext } from "react-query";
 import { weatherApiKey, whoIsApiKey } from "../constants/apiKeys";
 
 export const getFoxImages = async (): Promise<any> =>
-  await axios.get(`https://randomfox.ca/floof/`).then((res) => res.data);
+  await axiosInstance.get(`randomfox.ca/floof/`).then((res) => res.data);
 
 export const getStoaPs = async (): Promise<any> =>
-  await axios
-    .get(`https://api.themotivate365.com/stoic-quote`)
+  await axiosInstance
+    .get(`api.themotivate365.com/stoic-quote`)
     .then((res) => res.data);
 
 export const getWeatherDetails = async (): Promise<any> =>
-  await axios
+  await axiosInstance
     .get(
-      `https://api.weatherbit.io/v2.0/current?lat=39.866667&lon=32.866667&key=${weatherApiKey}`
+      `api.weatherbit.io/v2.0/current?lat=39.866667&lon=32.866667&key=${weatherApiKey}`
     )
     .then((res) => res.data);
 
 export const getTime = async (): Promise<any> =>
-  await axios
-    .get(`https://worldtimeapi.org/api/timezone/Europe/Istanbul`)
+  await axiosInstance
+    .get(`worldtimeapi.org/api/timezone/Europe/Istanbul`)
     .then((res) => res.data);
 
 export const getDangerousAddresses = async (): Promise<any> =>
-  await axios
-    .get(`https://www.usom.gov.tr/api/address/index`)
+  await axiosInstance
+    .get(`www.usom.gov.tr/api/address/index`)
     .then((res) => res.data);
 
 export const getRegInfs = async ({
   queryKey,
 }: QueryFunctionContext<[string, number | null]>): Promise<any> => {
   const [, domainName] = queryKey;
-  return await axios
-    .get(`https://api.ip2whois.com/v2?key=${whoIsApiKey}&domain=${domainName}`)
+  return await axiosInstance
+    .get(`api.ip2whois.com/v2?key=${whoIsApiKey}&domain=${domainName}`)
     .then((res) => res.data);
 };
